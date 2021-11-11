8c3c109a8a0c6ab68ae960f8629279155a33d915
pluig <pluig@LAPTOP-FTUUF4D0>
2021-11-11 18:17:22 -0500

    Proyecto sencillo

---
+++ src/HolaMundo/HolaMundo1.java
@@ -0,0 +1,10 @@
+
+package HolaMundo;
+
+public class HolaMundo1 {
+    public static void main (String [] args){ // El metodo/funcion main es impresindible, sin el, el codigo no se ejecutaria
+        
+        System.out.println("Hola Mundo En JAVA"); // Una de las formas de imprimir mensajes en pantalla en java es através del System.out.print
+
+    }
+}
